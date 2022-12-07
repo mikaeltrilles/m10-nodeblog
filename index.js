1ee7af28182e87d181ee10fad9c590af1a4f3246
Mikael Trilles <mikaeltrilles@gmail.com>
2022-12-07 14:55:31 +0100

    📦 Mise en place de EJS avec le répertoire views

--- index.js
+++ index.js
@@ -5,26 +5,35 @@ const express = require('express');
 
 // Declaration of the variables
 const path = require('path');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
 // Declaration of the static folder
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-// Declaration of the routes
+
+// Declarartion of the views
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Rendering of the views
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
-})
-app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
-})
-app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
-})
-app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-})
+  res.render('index');
+});
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 // Listening to the port
 app.listen(port, () => {
   console.log(`Blog listening at http://localhost:${port}`);
