b99d810d224b09e29ca009961ce8dc76df37dd0d
Mikael Trilles <mikaeltrilles@gmail.com>
2022-12-07 12:55:40 +0100

    🚀 Init projet NodeJS + Blog en statique

--- index.js
+++ index.js
@@ -8,31 +8,6 @@ const port = 3000;
 app.use(express.static(__dirname + '/public'));
 
 
-
-
-// Routes des pages dynamiques dans le dossier "pages"
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-})
-app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'about.html'));
-})
-app.get('/about-json', (req, res) => {
-  res. json ({
-    content: "About page content !"
-  });
-})
-app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'contact.html'));
-})
-app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'post.html'));
-})
-
-
-
-
-
 // Ecoute du serveur sur le port 3000
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
