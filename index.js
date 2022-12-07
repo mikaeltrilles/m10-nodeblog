6e5112731451be9daca39344bb2f380a8036bf0a
Mikael Trilles <mikaeltrilles@gmail.com>
2022-12-07 12:49:18 +0100

    💉 Fix fichier macOS dans le gitignore

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+// Fichier principal du serveur Node de l'application, déclaration du 'path' et du 'port' du serveur
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+// Routes des fichiers statiques dans le dossier "public"
 app.use(express.static(__dirname + '/public'));
+
+
+
+
+// Routes des pages dynamiques dans le dossier "pages"
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 })
@@ -21,6 +28,12 @@ app.get('/contact', (req, res) => {
 app.get('/post', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'post.html'));
 })
+
+
+
+
+
+// Ecoute du serveur sur le port 3000
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 })
